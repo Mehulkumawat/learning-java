bad5dfcdbd45b249541205138bb2f39bc7d972bf
Mehulkumawat <mehkumawat@gmail.com>
2020-10-06 21:49:35 +0530

    assignment 1

---
+++ addition.java
@@ -0,0 +1,11 @@
+public class addition{
+
+    public static void main(String[] args){
+        int a, b, c;
+        a = 4;
+        b = 5;
+        c = a + b;
+        System.out.println(c);
+
+    }
+}
---
+++ multiplication.java
@@ -0,0 +1,10 @@
+public class multiplication {
+    public static void main(String[] args){
+        int a, b, c;
+        a = 4;
+        b = 5;
+        c = a * b;
+        System.out.println(c);
+
+    }
+}
