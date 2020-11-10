5442b794be0f54f97b23c7eff273b623393ab35c
Mehulkumawat <mehkumawat@gmail.com>
2020-11-10 23:40:24 +0530

    ipaddress coding question

---
+++ coding/Defangedip.java
@@ -0,0 +1,17 @@
+package coding;
+
+public class Defangedip {
+    public static void main(String args) {
+        defanged(mehul);
+    }
+
+    public void defanged(String inputAddress) { 
+        // String outputAddress;
+        for(int i = 0; i < inputAddress.length; i++) {
+            system.out.println("hello");
+            
+
+        }
+    }
+    
+}
