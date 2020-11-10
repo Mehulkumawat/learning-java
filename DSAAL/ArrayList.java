1b937274beb464e3ce206d02acc55a546954a4e8
Mehulkumawat <mehkumawat@gmail.com>
2020-11-10 23:35:28 +0530

    arraylist implementation

---
+++ DSAAL/ArrayList.java
@@ -0,0 +1,44 @@
+package DSAAL;
+
+public class ArrayList {
+    private int[] array = new int[8]; // array ki length
+    private int size = 0;
+    
+    public int getsize() {
+        return size;
+    }
+
+    public int add(int value) {
+        if(size >= array.length)
+            increaselength()
+
+        array[size++] = value;   
+    }
+
+    public int increaselength() {
+        
+        
+    }
+
+    public void addAll(int[] value){
+        for(int i = 0; i < value.length; i++) {
+            add(value[i]);
+        }
+    }
+
+    public int get(int index) {
+        if(index >= size || index < 0)
+            throw new IndexOutOfBoundsException("index should be between [0, size)")
+        return array[index];
+    }
+
+    public int removeLast() {
+        if(size == 0) {
+            throw new IndexOutOfBoundsException("size is zero ,no further remving possible");
+        }
+        return array[--size];
+
+    }
+}
+    
+
